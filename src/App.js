6c7e0ab95e4fc10e428af6d1abdba6b62073921f
allreddv <allreddv@gmail.com>
2023-04-21 13:02:45 -0700

    Before adding Nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,70 @@
-import './App.css';
+import './index.css';
 import Employee from './ components/Employee';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import AddEmployee from './ components/AddEmployee';
+import EditEmployee from './ components/EditEmployee';
 
 function App() {
   const [role, setRole] = useState('dev');
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      name: 'Caleb',
+      role: 'Developer',
+      img: 'https://images.pexels.com/photos/762080/pexels-photo-762080.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    {
+      id: 2,
+      name: 'Caleb',
+      role: 'test',
+      img: 'https://images.pexels.com/photos/762080/pexels-photo-762080.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    {
+      id: 3,
+      name: 'Caleb',
+      role: 'Developer',
+      img: 'https://images.pexels.com/photos/762080/pexels-photo-762080.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    {
+      id: 4,
+      name: 'Caleb',
+      role: 'Developer',
+      img: 'https://images.pexels.com/photos/762080/pexels-photo-762080.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    {
+      id: 5,
+      name: 'Caleb',
+      role: 'Developer',
+      img: 'https://images.pexels.com/photos/762080/pexels-photo-762080.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+    {
+      id: 6,
+      name: 'Caleb',
+      role: 'Developer',
+      img: 'https://images.pexels.com/photos/762080/pexels-photo-762080.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    },
+  ]);
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
+  function newEmployee(name, role, img) {
+    const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img,
+    };
+    setEmployees([...employees, newEmployee]);
+  }
   const showEmployees = true;
   return (
     <div className="App">
@@ -16,9 +77,29 @@ function App() {
               setRole(e.target.value);
             }}
           />
-          <Employee name="caleb" role="Intern" />
-          <Employee name="Abby" role={role} />
-          <Employee name="John" />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              const editEmployee = (
+                <EditEmployee
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  updateEmployee={updateEmployee}
+                />
+              );
+              return (
+                <Employee
+                  key={employee.id}
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                  editEmployee={editEmployee}
+                />
+              );
+            })}
+          </div>
+          <AddEmployee newEmployee={newEmployee} />
         </>
       ) : (
         <p>You cannot see emplyees</p>
